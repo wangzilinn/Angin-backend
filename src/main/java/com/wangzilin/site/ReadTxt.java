db8fcf3e80d49e667f562b6eab7dbe8c8ada86a3
Wangzilin <wangzilinn@gmail.com>
2019-12-14 23:03:39 +0800

    成功读取了单词本

--- src/main/java/com/wangzilin/site/ReadTxt.java
+++ src/main/java/com/wangzilin/site/ReadTxt.java
@@ -2,22 +2,33 @@ package com.***REMOVED***.site;
 
 import org.springframework.stereotype.Controller;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 //@Controller
 public class ReadTxt {
     public void read() throws Exception{
-        String pathname = "C:\\Users\\78286\\Desktop\\input.txt"; // 绝对路径或相对路径都可以，这里是绝对路径，写入文件时演示相对路径
+        String pathname = "C:\\Users\\78286\\Desktop\\input1.txt"; // 绝对路径或相对路径都可以，这里是绝对路径，写入文件时演示相对路径
         File filename = new File(pathname); // 要读取以上路径的input。txt文件
         InputStreamReader reader = new InputStreamReader(
                 new FileInputStream(filename)); // 建立一个输入流对象reader
         BufferedReader br = new BufferedReader(reader); // 建立一个对象，它把文件内容转成计算机能读懂的语言
         String line = "";
+        System.out.println("测试");
+//        byte bt;
+//        while (true) {
+//
+//        }
+//        System.out.println(new String(fileInputStream.readAllBytes()));
+        int i = 0;
         while (line != null) {
             line = br.readLine(); // 一次读入一行数据
             System.out.println(line);
+            i++;
+            if (i > 10) {
+                break;
+            }
         }
     }
 }
