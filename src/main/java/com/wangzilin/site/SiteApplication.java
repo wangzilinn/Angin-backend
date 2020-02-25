a38e2eae8416bebce85011968a921ac12a26a4fd
Wangzilin <wangzilinn@gmail.com>
2020-02-25 23:35:07 +0800

    解决了部署到docker失败的问题

--- src/main/java/com/wangzilin/site/SiteApplication.java
+++ src/main/java/com/wangzilin/site/SiteApplication.java
@@ -6,8 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = MongoAutoConfiguration.class)
 public class SiteApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
