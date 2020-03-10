d0958748ca21cfdeb3766205bbb8e445708d5454
Wangzilin <wangzilinn@gmail.com>
2020-03-10 19:26:48 +0800

    实现了https访问 实现了mqtts

--- src/main/java/com/wangzilin/site/integration/Mqtt.java
+++ src/main/java/com/wangzilin/site/integration/Mqtt.java
@@ -4,16 +4,20 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.***REMOVED***.site.model.MessageModel;
 import com.***REMOVED***.site.services.MessageService;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
 import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
 import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
+
+import javax.net.ssl.SSLSocketFactory;
 
 @Configuration
 public class Mqtt {
@@ -29,9 +33,14 @@ public class Mqtt {
     }
 
     @Bean
-    public MessageProducer inbound() {
+    public MessageProducer inbound() throws Exception {
+        DefaultMqttPahoClientFactory defaultMqttPahoClientFactory = new DefaultMqttPahoClientFactory();
+        MqttConnectOptions options = new MqttConnectOptions();
+        SSLSocketFactory socketFactory = SslUtil.getSocketFactory();
+        options.setSocketFactory(socketFactory);
+        defaultMqttPahoClientFactory.setConnectionOptions(options);
         MqttPahoMessageDrivenChannelAdapter adapter =
-                new MqttPahoMessageDrivenChannelAdapter("tcp://***REMOVED***:1883", "tableText",
+                new MqttPahoMessageDrivenChannelAdapter("ssl://***REMOVED***:8883", "backend", defaultMqttPahoClientFactory,
                         "chat");
         adapter.setCompletionTimeout(30000);
         adapter.setConverter(new DefaultPahoMessageConverter());
