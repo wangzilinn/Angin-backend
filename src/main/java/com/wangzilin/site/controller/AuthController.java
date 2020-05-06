c97c266242fc24cb1153c8d174bfdb3b956d6de0
Wangzilin <wangzilinn@gmail.com>
2020-05-06 22:55:39 +0800

    完成了用户验证controller,接下来是最难的article controller

--- src/main/java/com/wangzilin/site/controller/AuthController.java
+++ src/main/java/com/wangzilin/site/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.***REMOVED***.site.controller;
 
+import com.***REMOVED***.site.model.reqresp.Response;
 import com.***REMOVED***.site.model.reqresp.SignRequest;
 import com.***REMOVED***.site.services.UserService;
+import io.swagger.annotations.Api;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +15,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
 @RequestMapping(value = "/auth")
+@Api(value = "AuthController", tags = "鉴权接口")
 public class AuthController {
 
 
@@ -37,16 +42,20 @@ public class AuthController {
      * @throws AuthenticationException .
      */
     @RequestMapping(value = "/signIn", method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity<String> SignIn(@Valid @RequestBody SignRequest authRequest, BindingResult bindingResult) throws AuthenticationException {
+    public Response SignIn(@Valid @RequestBody SignRequest authRequest, BindingResult bindingResult) throws AuthenticationException {
 
         if (bindingResult.hasErrors()) {
             bindingResult.getFieldErrors().forEach((fieldError) -> log.info(fieldError.getField()));
-            return new ResponseEntity<>("输入格式错误", HttpStatus.UNPROCESSABLE_ENTITY);
+            return new Response<>(422, "输入格式错误");
         }
 
         final String token = userService.signIn(authRequest.getUserId(), authRequest.getPassword());
 
-        return new ResponseEntity<>(token, HttpStatus.OK);
+        // 变为字典格式如:{"token":"e77e2268-7252-4b29-b606-acbca981836d"}} 输出
+        Map<String, Object> map = new HashMap<>();
+        map.put("token", token);
+
+        return new Response<>(map);
     }
 
 
