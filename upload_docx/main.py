6dd21c39807c10af45ccd4455f3831adbf63edfd
Wangzilin <wangzilinn@gmail.com>
2020-06-27 22:24:43 +0800

    实现了docx转html并测试， 实现了上传图片到mongodb

--- upload_docx/main.py
+++
@@ -1,9 +0,0 @@
-import win32com.client
-
-word = win32com.client.Dispatch('Word.Application')
-
-doc = word.Documents.Add(r"C:\Case\case-Java\191212_angin_backend\upload_docx\test.docx")
-doc.SaveAs(r'C:\Case\case-Java\191212_angin_backend\upload_docx\test.html', FileFormat=8)
-doc.Close()
-
-word.Quit()
