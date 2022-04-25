ae0dd617a48c4c24b9342d4fe91ad150d97532db
wangzilin <wangzilinn@gmail.com>
2022-04-25 22:27:38 +0800

    增加了lifelog相关的内容，不过因为涉及隐私，所以大部分都被git ignore了

---
+++ db/lifeLog.sql
@@ -0,0 +1,30 @@
+use
+    `common`;
+
+set names utf8;
+drop table if exists `lifelog`;
+create table `lifelog`
+(
+    `id`        bigint(20)   not null auto_increment,
+    `type_id`   varchar(100) not null,
+    `type_name` varchar(100) not null,
+    `date`      DATETIME default null,
+    primary key (`id`)
+) engine = InnoDB
+  auto_increment = 1
+  default charset = utf8;
+
+
+set names utf8;
+drop table if exists `lifelog_summary`;
+create table `lifelog_summary`
+(
+    `id`           bigint(20)   not null auto_increment,
+    `type_name`    varchar(100) not null,
+    `last_date`    DATETIME   default null,
+    `duration`     bigint(20) default null,
+    `max_duration` bigint(20) default null,
+    primary key (`id`)
+) engine = InnoDB
+  auto_increment = 1
+  default charset = utf8;
