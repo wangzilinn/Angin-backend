0729f925971c76bb7812ede70e0a5b668dfc3c71
zillionwang <wangzilinn@gmail.com>
2022-08-28 19:36:35 +0800

    commit test

--- upload_article/convert_docx_to_html.py
+++ upload_article/convert_docx_to_html.py
@@ -5,4 +5,4 @@ def convert_docx_to_html(file_src, dest):
     subprocess.check_output(
         ["/Applications/LibreOffice.app/Contents/MacOS/soffice", "--headless", "--convert-to", "html", file_src,
          "--outdir", dest])
-    print('success!')
+    print('convert success!')
